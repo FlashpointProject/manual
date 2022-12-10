d9e0d4ff0b2007957e786643cf595d8b44aad0cb
Wumbo <58399748+WumboSpasm@users.noreply.github.com>
2022-12-10 14:38:08 -0500

    Revert previous change, replace with improved JS

--- docs/page.js
+++ docs/page.js
@@ -1,18 +1,15 @@
-let query = new URLSearchParams(location.search);
-if (query.has('page')) {
-    // Embed requested document
-    let pageRequest = new XMLHttpRequest();
-    pageRequest.open('GET', 'text/' + query.get('page') + '.html');
-    pageRequest.responseType = 'text';
-    pageRequest.send();
-    pageRequest.onload = function() {
-        document.querySelector('.text').innerHTML = this.response;
-        // Highlight link to requested document
-        document.querySelector('.sidebar a[href="?page=' + query.get('page') + '"]').style.fontWeight = 'bold';
-        // Change title of page
-        let title = document.querySelector('.sidebar a[href="?page=' + query.get('page') + '"]').textContent;
-        document.querySelector('title').textContent = title + ' - Flashpoint Help Manual';
-    };
-}
-else
-    window.location.replace('?page=Welcome');
+let query = new URLSearchParams(location.search),
+    pageID = query.has('page') ? query.get('page') : 'Welcome';
+// Embed requested document
+let pageRequest = new XMLHttpRequest();
+pageRequest.open('GET', 'text/' + pageID + '.html');
+pageRequest.responseType = 'text';
+pageRequest.send();
+pageRequest.onload = function() {
+    document.querySelector('.text').innerHTML = this.response;
+    // Highlight link to requested document
+    document.querySelector('.sidebar a[href="?page=' + pageID + '"]').style.fontWeight = 'bold';
+    // Change title of page
+    let title = document.querySelector('.sidebar a[href="?page=' + pageID + '"]').textContent;
+    document.querySelector('title').textContent = title + ' - Flashpoint Help Manual';
+};
