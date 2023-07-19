e12ec621258dc0d1ad49a6c2608b6e5ebc11842d
Wumbo <58399748+WumboSpasm@users.noreply.github.com>
2023-07-18 21:50:18 -0400

    Update to reflect changes in Flashpoint 12

--- docs/page.js
+++ docs/page.js
@@ -1,15 +1,11 @@
 let query = new URLSearchParams(location.search),
     pageID = query.has('page') ? query.get('page') : 'Welcome';
 // Embed requested document
-let pageRequest = new XMLHttpRequest();
-pageRequest.open('GET', 'text/' + pageID + '.html');
-pageRequest.responseType = 'text';
-pageRequest.send();
-pageRequest.onload = function() {
-    document.querySelector('.text').innerHTML = this.response;
+fetch(`text/${pageID}.html`).then(r => r.text()).then(html => {
+    document.querySelector('.text').innerHTML = html;
     // Highlight link to requested document
     document.querySelector('.sidebar a[href="?page=' + pageID + '"]').style.fontWeight = 'bold';
     // Change title of page
     let title = document.querySelector('.sidebar a[href="?page=' + pageID + '"]').textContent;
     document.querySelector('title').textContent = title + ' - Flashpoint Help Manual';
-};
+});
